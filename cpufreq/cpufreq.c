a485b1395979715d63630c014b38112a1db67636
ng <nicolas@nghs.fr>
2007-03-31 11:58:59 +0000

    added license terms
    git-svn-id: file:///Users/ng/svn-local/to_share/utils@7 54a496e3-1623-4b03-9353-7eaf590d67d4

--- cpufreq/cpufreq.c
+++ cpufreq/cpufreq.c
@@ -1,3 +1,19 @@
+/***************************************************************************
+ *   This file is part of the 'utils' projects                             *
+ *                                                                         *
+ *   'utils' projects                                                      *
+ *                                                                         *
+ *   Copyright (C) 2006, 2007 by GUILLAUME Nicolas                         *
+ *   ng@ngsoft-fr.com                                                      *
+ *                                                                         *
+ *   http://www.ngsoft-fr.com/                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License only.          *
+ *   See the COPYING file.                                                 *
+ ***************************************************************************/                                                                
+
 
 /**
   * Author: Nicolas GUILLAUME <ng@ngsoft-fr.com>

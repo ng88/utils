28fee4826065c16a8f3d140985862e0c0e05cfe8
ng <nicolas@nghs.fr>
2007-03-31 10:04:03 +0000

    * adding option support
    git-svn-id: file:///Users/ng/svn-local/to_share/utils@4 54a496e3-1623-4b03-9353-7eaf590d67d4

--- xoror/main.c
+++ xoror/main.c
@@ -1,6 +1,8 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 #include "xoror.h"
 
 
@@ -8,12 +10,82 @@
  
      Compile with:  gcc -Wall xoror.c main.c -o xoror
 
-*/ 
+*/
+
+
+/* -h -p blabla -k 456 -s salut|-i file1 -o file2*/
+void usage(const char * pname)
+{
+}
 
 int main(int argc, char ** argv)
 {
 
-    cryptor * c = cryptor_new(argv[1], atoi(argv[2]));
+    char * pname = strrchr(argv[0], '/');
+    if( !pname )
+        pname = argv[0];
+    else
+	pname++;
+
+    int optch;
+
+    int key = 0;
+    char * pass = NULL;
+    char * string = NULL;
+    FILE * in = NULL;
+    FILE * out = NULL;
+
+    while( (optch = getopt(argc, argv, "p:k:s:hi:o:")) != -1 )
+    {
+	switch(optch)
+	{
+	case 'i':
+	    in = fopen(optarg, "rb");
+	    if(!in)
+	    {
+		fprintf(stderr, "%s: unable to open file `%s'\n", pname, optarg);
+		return EXIT_FAILURE;
+	    }
+	    break;
+	case 'o':
+	    out = fopen(optarg, "wb");
+	    if(!in)
+	    {
+		fprintf(stderr, "%s: unable to open file `%s'\n", pname, optarg);
+		return EXIT_FAILURE;
+	    }
+	    break;
+	case 'i':
+	    pass = optarg;
+	    break;
+	case 'k':
+	    key = atoi(optarg);
+	    break;
+	case 's':
+	    string = optarg;
+	    break;
+	case 'h':
+	    usage(pname);
+	    break;
+	default:
+	    break;
+	}
+    }
+
+    if( (in || out) && string)
+	usage(pname);
+
+    if( !pass || strlen(pass) <= 2)
+	usage(pname);
+
+    if(!key)
+	key = pass[0] * pass[1];
+
+    printf("%d\n", argc - optind);
+
+    return EXIT_SUCCESS;
+
+    /*cryptor * c = cryptor_new(argv[1], atoi(argv[2]));
 
     FILE * in = fopen(argv[3], "rb");
     FILE * out = fopen(argv[4], "wb");
@@ -26,6 +98,6 @@ int main(int argc, char ** argv)
 
     cryptor_free(c);
 
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS;*/
 }
 

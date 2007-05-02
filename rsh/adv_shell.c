2ae2097ff4f5f8e0953ecb5ab4d5cceeaea0463f
ng <nicolas@nghs.fr>
2007-05-02 15:20:05 +0000

    moved io to adv_shell
    git-svn-id: file:///Users/ng/svn-local/to_share/utils@23 54a496e3-1623-4b03-9353-7eaf590d67d4

--- rsh/adv_shell.c
+++ rsh/adv_shell.c
@@ -14,71 +14,42 @@
  *   See the COPYING file.                                                 *
  ***************************************************************************/                                                                
 
-/**
 
-   Author: Nicolas GUILLAUME <ng@ngsoft-fr.com>
-
-   rsh is a restricted shell that can execute only few commands.
-   It is typically used on a ssh server for user with restricted privilege.
-
-   compile with: gcc -Wall conf.c  main.c  rsh.c -o rsh
-
-*/
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#include "rsh.h"
-#include "conf.h"
 #include "adv_shell.h"
 
 
-int main(int argc, char ** argv)
+#ifndef ADV_READLINE
+
+# define _GNU_SOURCE
+
+# include <stdio.h>
+# include <string.h>
+
+char * line_input(char * prompt)
 {
-    command_list_t commands;
-    char * message;
-    char * prompt;
-    FILE * log;
+    char * line = NULL;
+    size_t len = 0;
 
-    read_conf(&prompt, &message, &commands, &log);
+    fputs(prompt, stdout);
 
-    if(log)
-	log_write_login(log);
+    if(getline(&line, &len, stdin) == -1)
+	return NULL;
 
-    if(argc == 3 && !strcmp(argv[1], "-c"))
-    {
-	exec_command(argv[2], commands, log);
-    }
-    else
-    {
-
-	puts(message);
-
-	char * line;
-
-	int cont = 1;
-	while(cont &&  (line = line_input(prompt)) )
-	{
-       
-	    if(!exec_command(line, commands, log))
-		cont = 0;
-	    
-	    free(line);
-
-	}
-
-    }
-
-    if(log)
-    {
-	log_write_logout(log);
-	fclose(log);
-    }
-
-    free_command_list(commands);
-    free(prompt);
-    free(message);
-
-    return EXIT_SUCCESS;
+    *(strchr(line, '\n')) = '\0';
+    return line;
 }
+
+#else
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+
+char * line_input(char * prompt)
+{using_history();
+    return readline(prompt);
+}
+
+#endif
+
--- rsh/adv_shell.h
+++ rsh/adv_shell.h
@@ -1,5 +1,22 @@
-#ifndef IO_H
-#define IO_H
+/***************************************************************************
+ *   This file is part of the 'utils' projects                             *
+ *                                                                         *
+ *   'utils' projects                                                      *
+ *                                                                         *
+ *   Copyright (C) 2006, 2007 by GUILLAUME Nicolas                         *
+ *   ng@ngsoft-fr.com                                                      *
+ *                                                                         *
+ *   http://www.ngsoft-fr.com/                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License only.          *
+ *   See the COPYING file.                                                 *
+ ***************************************************************************/                                                                
+
+
+#ifndef ADV_SHELL_H
+#define ADV_SHELL_H
 
 char * line_input(char * prompt);
 

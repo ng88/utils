3920f8f68ae22f686d3bdde39754c3dd727cfff6
ng <nicolas@nghs.fr>
2007-12-22 23:25:21 +0000

    added script that can add user easily to the server
    git-svn-id: file:///Users/ng/svn-local/to_share/utils@75 54a496e3-1623-4b03-9353-7eaf590d67d4

---
+++ btun/add_user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" -o ! -w "$1" ]
+then
+    echo "usage: $0 user_file" >&2
+    exit 2
+fi
+
+read -p "Login: " login
+
+stty -echo
+read -p "Password: " pass; echo
+stty echo
+
+echo $login:$(echo -n $pass | md5sum | cut -f1 -d" ") >> $1
+
+echo Added.

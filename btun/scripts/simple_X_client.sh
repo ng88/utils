7a8b4e0f500ecf24cc35784a49d4b93d7b5426c5
ng <nicolas@nghs.fr>
2007-12-27 12:45:18 +0000

    added simple X tunnel creation scripts
    git-svn-id: file:///Users/ng/svn-local/to_share/utils@90 54a496e3-1623-4b03-9353-7eaf590d67d4

---
+++ btun/scripts/simple_X_client.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+#  Author: Nicolas GUILLAUME <ng@ngsoft-fr.com>
+#
+# btun_X_tunnel_client.sh & btun_X_tunnel_server.sh enable you to execute
+# X application through a btun tunnel.
+#
+#
+#
+#                      btund
+#                    /       \               
+#                   /         \              
+#                  /           \            
+#                 /             \           
+#                /               \          
+#               |                |         
+#      /      btun              btun      \   
+#      |        |                |        |         
+# (1) <        nc                nc        > (2)
+#      |        |                |        | 
+#      \  local X server    remote X app  /                  
+#      
+#
+#   (1) simple_X_client.sh: btun + nc + local X server connection
+#   (2) simple_X_server.sh: btun + nc + X app connection on remote host
+#
+#
+
+
+if [ -z "$1" -o -z "$2" ]
+then
+    echo "usage: $0 user@host channel_prefix [btun option]" >&2
+fi
+
+user="$1"
+channel="$2"
+option="$3"
+
+xhost +localhost
+
+btun "$user" "$3" "${channel}_X_1" -- nc localhost 6000

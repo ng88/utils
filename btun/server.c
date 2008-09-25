4a446824f39788c58695567c5f247906dba59aeb
ng <nicolas@nghs.fr>
2008-09-25 13:01:51 +0000

    * moving to IPv6
    git-svn-id: file:///Users/ng/svn-local/to_share/utils@191 54a496e3-1623-4b03-9353-7eaf590d67d4

--- btun/server.c
+++ btun/server.c
@@ -20,7 +20,7 @@
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; version 2 of the License only.          *
  *   See the COPYING file.                                                 *
- ***************************************************************************/                                                                
+ ***************************************************************************/
 
 
 #include "server.h"
